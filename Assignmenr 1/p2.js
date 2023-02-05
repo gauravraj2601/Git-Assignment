125434c49c4d5fe1854b9ac91dc95c366779c03d
gauravraj2601 <gauravraj2601@gmail.com>
2023-02-05 21:46:40 +0530

    first_commit

---
+++ Assignmenr 1/p2.js
@@ -0,0 +1,6 @@
+let name="Gaurav Raj";
+console.log(name);
+let father= "Kishori Prasad";
+console.log(father);
+let mother= "Savitri Devi";
+console.log(mother);
---
+++ Assignmenr 1/p4.js
@@ -0,0 +1,14 @@
+let name="Gaurav Raj";
+let school= "Masai School";
+let grade="A";
+let section="Web26";
+let roll="fp11_060";
+// Marks scored
+let maths=85;
+let science=80;
+let english=75;
+console.log("Name-",name);
+console.log("School-",school);
+console.log("Grade-",grade);
+console.log("Section-",section,"& Roll-",roll);
+console.log("Maths-",maths,", Science-",science,"and English-",english);
