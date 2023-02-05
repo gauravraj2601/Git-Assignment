9b8456e4fa5e1df297e89e8f1a2e846131315fc9
gauravraj2601 <gauravraj2601@gmail.com>
2023-02-05 22:24:09 +0530

    day_2

---
+++ Mathematical operators/prac1.js
@@ -0,0 +1,22 @@
+//data types
+//concatenation
+console.log("hello"+"\n"+"world");
+console.log(2<3); //true
+console.log(7<7); //false
+console.log(4<2); //false
+console.log(4<=4); //true
+console.log(2<=4); //true
+console.log(5<=3); //false
+
+console.log(2>5); //false
+console.log(4>2); //true
+console.log(4>4); //false
+
+console.log(2>=5); //false
+console.log(4>=2); //true
+console.log(4>=4); //true
+
+console.log(10==="10") //false (value is same but the data type is different)("masai"==="Masai") false
+
+console.log("2"!==2) //true (values are same but the data types are diff.)("2"!==5)  value "OR"  data type are not equal.
+console.log("2"!=="2") //false  values and data type are equals(both).
---
+++ Mathematical operators/prac2.js
@@ -0,0 +1,4 @@
+let golu_age=20;
+let driving_age=21;
+let eligible=golu_age >= driving_age;
+console.log(eligible)
