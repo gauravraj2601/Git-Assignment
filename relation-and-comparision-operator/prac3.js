f64269efcc8ff6d6e984825e98a199b5116c8268
gauravraj2601 <gauravraj2601@gmail.com>
2023-02-05 22:35:32 +0530

    day3

---
+++ relation-and-comparision-operator/prac3.js
@@ -0,0 +1,11 @@
+// we compare strings with "===, ==, //!=, !===" only.
+let a= 10;
+let b= 8;
+// if (a>b){
+//   console.log('a is greater')
+// }
+// else{
+//   console.log("b is greater")
+// }
+
+(a>b) ? console.log("a is greater") : (b>a) ? console.log("b is greater") : (a==b) ? console.log("a is equal b")
